bdc8b244a05b6f3e6255e769a3a14347c3ee7a5f
darren2581 <darrentanthongen@gmail.com>
2024-11-14 15:15:02 +0800

    change file name 2.0

--- src/components/RegistrationPage.js
+++ src/components/RegistrationPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/HomePage.css';
+import '../styles/RegistrationPage.css';
 
 const HomePage = () => {
   return (
